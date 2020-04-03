d8bcafcfb69b6295be67072b4ddbcfc8df8b6a68
Lilian Korir <likorir@davidson.edu>
2020-04-03 10:59:08 -0400

    revesions

--- src/smallestPrimeFactor.js
+++ src/smallestPrimeFactor.js
@@ -12,47 +12,24 @@
  * @returns {number} The smallest prime factor of num
  */
 
-  function isPrime(num) {
-    if ( num === 1){
-       return false;}
-
-       if ( num === 2){return true;}
-
-     for ( let n = 2; n < num; n++){
-     if (num % n === 0){
-      return false;
-     }
-     return true;
-   }
-  }
-
   function smallestPrimeFactors(num) {
-    let array = [];
-    let i = 2; // i-quotient
     for (i=2; i<= num; i++){
-  if (num % i === 0){
-    if (isPrime(i) === true)
-      array.push(i);
-      }
-  }
-  return Math.min(array[0]);
-  // This is your job. :)
+if (num % i === 0){
+  return i;}
 }
-
+  }
 if (require.main === module) {
   console.log('Running sanity checks for smallestPrimeFactor:');
-  console.log(smallestPrimeFactors(2)); // => [2]
-  console.log(smallestPrimeFactors(255)); // => [2]
-  console.log(smallestPrimeFactors(13*17*29)); // => [2, 2]
-  console.log(smallestPrimeFactors(2 * 2 * 3)); // => [2, 2, 3]
-  console.log(smallestPrimeFactors(917329)); // => [2, 2, 3, 17, 197]
-  console.log(smallestPrimeFactors(125)); // => [7, 7, 97, 193]
-  console.log(smallestPrimeFactors(21)); // => [7, 7, 97, 193]
+  console.log(smallestPrimeFactors(2)); // => 2
+  console.log(smallestPrimeFactors(255)); // => 3
+  console.log(smallestPrimeFactors(13*17*29)); // => 13
+  console.log(smallestPrimeFactors(2 * 2 * 3)); // => 2
+  console.log(smallestPrimeFactors(917329)); // => 7
+  console.log(smallestPrimeFactors(125)); // => 5
+  console.log(smallestPrimeFactors(21)); // => 3
 
   // Add your own sanity checks here.
   // How else will you be sure your code does what you think it does?
-}
 
+  }
 module.exports = smallestPrimeFactors;
-
-//module.exports = smallestPrimeFactor;
